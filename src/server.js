da328f0dba663e2eb5ae72858f801f9a96dca76b
Eduardo <sandrell560@gmail.com>
2024-06-14 19:31:53 -0300

    update-14-06-2024

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
 const express = require('express')
-
 const app = express()
+const routes = require('./routes')
 
-app.get('/message/:id/:user', (request, response) => {
-  const {id, user} = request.params 
-  response.send(`
-    ID da mensagem: ${id}
-    Nome do usuário: ${user}
-    `)
-})
+app.use(express.json())
+app.use(routes)
 
-app.get('/users', (request, response) => {
-  const { page, limit} = request.query
-
-  response.send(`Quantidade de páginas: ${page} 
-    O limite destas: ${limit}`)
+app.post('/users', (request, response) => {
+  const {name, email, password} = request.body
+  response.json({name, email, password})
 })
 
 const PORT = 3333
-
 app.listen(PORT, () => console.log(`Funcionando na porta ${PORT}`))
