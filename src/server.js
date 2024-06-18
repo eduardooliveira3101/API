6c1c6249a517cd91ee32c4f3a2ff3626563ad2c2
Eduardo <sandrell560@gmail.com>
2024-06-18 20:22:59 -0300

    add migration sqlite SGBD

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require('express-async-errors')
-const database = require('./database/sqlite')
+const migrationsRun = require('./database/sqlite/migrations')
 const express = require('express')
 const routes = require('./routes')
 const AppError = require('./utils/AppError')
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-database()
+migrationsRun()
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
